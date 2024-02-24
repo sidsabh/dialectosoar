aec41981471f4bc07eea64157ba27d249577a377
Sid Sabhnani <ssabhnani@utexas.edu>
2024-02-24 15:57:32 -0600

    fix

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,21 @@
 // server/index.js
-
+import { getSubtitles, getVideoDetails } from 'youtube-caption-extractor';
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
+app.get("/video-data", async (req, res) => {
+  const { videoID, lang } = req.query;
+
+  try {
+    const subtitles = await getSubtitles({ videoID, lang }); // call this if you only need the subtitles
+    const videoDetails = await getVideoDetails({ videoID, lang }); // call this if you need the video title and description, along with the subtitles
+    res.status(200).json({ subtitles, videoDetails });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
   
 app.listen(PORT, () => {
-console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
